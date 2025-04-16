210a6287fff85198f7fc0c339e131cb9276a2185
cinis <bartlomiejdabrowski@gmail.com>
2025-04-16 17:32:08 +0200

    classByYearId endpoint fix

--- src/main/java/com/umcs/skni/mordappka/classes/ClassController.java
+++ src/main/java/com/umcs/skni/mordappka/classes/ClassController.java
@@ -3,6 +3,7 @@ package com.umcs.skni.mordappka.classes;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,13 +12,14 @@ import java.util.List;
 
 
 @RequiredArgsConstructor
-@RestController("/class")
+@RestController
+@RequestMapping("/class")
 public class ClassController {
 
     private final ClassService classService;
 
     @GetMapping("/year")
-    public ResponseEntity<List<ClassDTO>> hello(@RequestParam Long id) {
+    public ResponseEntity<List<ClassDTO>> getClassesByYear(@RequestParam Long id) {
         return ResponseEntity.ok(classService.getClassesByYearId(id));
     }
 }
