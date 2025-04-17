b97cd729018dc4f48b5a4d1ce4b6de8c65d32475
cinis <bartlomiejdabrowski@gmail.com>
2025-04-17 10:17:01 +0200

    formatowanie

--- src/main/java/com/umcs/skni/mordappka/teacher/TeacherDTO.java
+++ src/main/java/com/umcs/skni/mordappka/teacher/TeacherDTO.java
@@ -1,9 +1,9 @@
 package com.umcs.skni.mordappka.teacher;
 
 public record TeacherDTO(
-        Long teacherId
-        , String teacherName
-        , String teacherDegree
-        , String facultyName
+        Long teacherId,
+        String teacherName,
+        String teacherDegree,
+        String facultyName
 ) {
 }
