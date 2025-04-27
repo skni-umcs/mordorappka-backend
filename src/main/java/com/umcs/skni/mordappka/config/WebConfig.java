6e080a570b4a00a0de9a146e447cbeec8895cceb
fuszz <uliaszfilip@gmail.com>
2025-04-27 12:04:25 +0200

    CORS fix

--- src/main/java/com/umcs/skni/mordappka/config/WebConfig.java
+++ src/main/java/com/umcs/skni/mordappka/config/WebConfig.java
@@ -10,20 +10,31 @@ public class WebConfig {
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
+//        return new WebMvcConfigurer() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+////                registry.addMapping("/**") // dla wszystkich endpointów
+////                        .allowedOrigins("http://localhost:3000") // frontend
+////                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+////                        .allowedHeaders("*")
+////                        .allowCredentials(true);
+//                //zmienic kiedys do produkcji
+//                    registry.addMapping("/**")
+//                            .allowedOrigins("*")
+//                            .allowedMethods("*")
+//                            .allowedHeaders("*")
+//                            .allowCredentials(false);
+//            }
+//        };
+
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-//                registry.addMapping("/**") // dla wszystkich endpointów
-//                        .allowedOrigins("http://localhost:3000") // frontend
-//                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-//                        .allowedHeaders("*")
-//                        .allowCredentials(true);
-                //zmienic kiedys do produkcji
-                    registry.addMapping("/**")
-                            .allowedOrigins("*")
-                            .allowedMethods("*")
-                            .allowedHeaders("*")
-                            .allowCredentials(false);
+                registry.addMapping("/**")
+                        .allowedOrigins("http://10.50.50.77:8080")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
             }
         };
     }
